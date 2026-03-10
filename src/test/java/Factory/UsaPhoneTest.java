edcb71e4c7e23945a6fa3b3048f56f9704a32868
Daniel Vilà <danivila.dev@gmail.com>
2026-03-10 10:47:44 +0100

    create phoneUsa test

--- src/test/java/Factory/UsaPhoneTest.java
+++ src/test/java/Factory/UsaPhoneTest.java
@@ -7,11 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UsaPhoneTest {
 
     @Test
-    void test_correct_format_UsaPhone_number(){
+    void test_correct_format_UsaPhone_number() {
 
         UsaPhone usaPhone = new UsaPhone("987654321");
         String formatUsaPhone = usaPhone.getFormatedPhoneNumber();
 
-        assertEquals("+1 (098) 765-4321",formatUsaPhone );
+        assertEquals("+1 (098) 765-4321", formatUsaPhone);
     }
 }
