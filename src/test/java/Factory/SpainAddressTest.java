e210674c40cf871da82b1a628a84e694b90aae53
Daniel Vilà <danivila.dev@gmail.com>
2026-03-10 10:32:35 +0100

    create addressSpain test

---
+++ src/test/java/Factory/SpainAddressTest.java
@@ -0,0 +1,18 @@
+package Factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpainAddressTest {
+
+    @Test
+    void test_correct_Format_Spain_address(){
+
+        SpainAddress directionSpain = new SpainAddress("Carrer Major 34", "Barcelona", "08001");
+        String formatSpaintDirection = directionSpain.getFormatedAdress();
+
+        assertEquals("Carrer Major 34, 08001 Barcelona", formatSpaintDirection);
+
+    }
+}
